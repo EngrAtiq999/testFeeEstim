4e26a7cf842d4532f66924edd40b059370eaeaac
EngrAtiq999 <134375706+EngrAtiq999@users.noreply.github.com>
2026-09-14 16:14:22 +0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'loadmeter-v1';
+const CACHE = 'trfee-v1';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (e) => {
